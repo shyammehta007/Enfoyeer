9ff77dcbce1b45cdae295ba043a3289beeb2b2eb
shyammehta007 <shyam.mehta@proptiger.com>
2020-07-20 15:11:22 +0530

    Updates

---
+++ precache-manifest.9be6e3d52da227f0f1a1bbc61bb9f642.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "233de42806d480e14b549ea72504d134",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e5dd5b30e670dad98fe",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "cfe89495a2da12830a1f",
+    "url": "/static/css/main.673adec3.chunk.css"
+  },
+  {
+    "revision": "1e5dd5b30e670dad98fe",
+    "url": "/static/js/2.460d1094.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/static/js/2.460d1094.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfe89495a2da12830a1f",
+    "url": "/static/js/main.93e08c38.chunk.js"
+  },
+  {
+    "revision": "89dfbc079c16efb3042a",
+    "url": "/static/js/runtime-main.65b3a13d.js"
+  },
+  {
+    "revision": "20debe4a62fe238aff104d38931b31ff",
+    "url": "/static/media/pageNotFound.20debe4a.png"
+  }
+]);
